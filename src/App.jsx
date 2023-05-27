95cb0494f85385c3dd7c7d34630d5b9a87384c0a
Jhon Luna <jhonlunaa56@gmail.com>
2023-05-26 19:38:17 -0500

    inciando

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Product from './components/Product';
+
+function App() {
+  return (
+    <>
+      <h1>MOBISTORE</h1>
+      <h2>cali</h2>
+      <login/>
+      <nav/>
+      <Product/>
+    </>
+  )
+}
+
+export default App
