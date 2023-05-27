f27200bb87e55f11246a74db576e721207aa8926
Michelle López <132721171+michelle27lopez@users.noreply.github.com>
2023-05-27 12:38:52 -0500

    nav

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faStar} from '@fortawesome/free-solid-svg-icons'
+import styled from'styled-components'
+
+function navbar(){
+return (
+<>
+<NavContainer>
+<h2> Hola soy el navbar <span>responsive</span></h2>
+</NavContainer>
+</>
+
+)
+
+}
+
+const NavContainer = styled.nav
